0df67721020bbd8e751db93a1883e5fc981a6a88
laridev@gmail.com <laridev@gmail.com>
2019-08-31 19:25:51 +0100

    adding flight table

--- src/flights/index.js
+++ src/flights/index.js
@@ -3,10 +3,64 @@ import { Table, Icon, Switch, Radio, Form, Divider } from 'antd';
 import './flights.scss';
 class index extends Component {
     render() {
+        const dataSource = [
+            {
+              key: '1',
+              Airline: 'Vueling',
+              Date: '03/11/2019',
+              Departure: 'Paris (CDG)',
+              Arrival: 'Miami',
+              Time: '08:45',
+              Price: '870$',
+            },
+            {
+                key: '2',
+                Airline: 'Vueling',
+                Date: '03/11/2019',
+                Departure: 'Paris (CDG)',
+                Arrival: 'Miami',
+                Time: '08:45',
+                Price: '870$',
+              }
+          ];
+          
+          const columns = [
+            {
+              title: 'Airline',
+              dataIndex: 'Airline',
+              key: 'Airline',
+            },
+            {
+              title: 'Date',
+              dataIndex: 'Date',
+              key: 'Date',
+            },
+            {
+              title: 'Departure',
+              dataIndex: 'Departure',
+              key: 'Departure',
+            },
+            {
+              title: 'Arrival',
+              dataIndex: 'Arrival',
+              key: 'Arrival',
+            },
+            {
+              title: 'Time',
+              dataIndex: 'Time',
+              key: 'Time',
+            },
+            {
+              title: 'Price',
+              dataIndex: 'Price',
+              key: 'Price',
+              render: text => <a className="price_tag">{text}</a>,
+            }
+          ];
         return (
-            <div>
-                hello i m flight
-            </div>
+            <section className="flights">
+                    <Table dataSource={dataSource} columns={columns} />;
+            </section>
         );
     }
 }
