08a2da6e99a80941bc6c9be536ee2681ec178cf3
laridev@gmail.com <laridev@gmail.com>
2019-08-31 15:04:39 +0100

    removing ant design from search bar :/

--- src/header/searchbar/index.js
+++ src/header/searchbar/index.js
@@ -1,24 +1,26 @@
 import React, { Component } from 'react';
-import { Input } from 'antd';
-import { Row, Col } from 'antd';
+import { Row, Col , Button , Input , DatePicker } from 'antd';
+
 import './searchbar.scss';
 class index extends Component {
+    
     render() {
+        const { MonthPicker, RangePicker, WeekPicker } = DatePicker;
         return (
              <section className="searchbar">
                 <div>
                     <Row>
                     <Col span={6}>
-                        <div className="gutter-box"> <Input placeholder="Basic usage" /></div>
+                        <div className="gutter-box"> <input placeholder="Where are you leaving from ?" /></div>
                     </Col>
                     <Col span={6}>
-                        <div className="gutter-box"> <Input placeholder="Basic usage" /></div>
+                        <div className="gutter-box"> <input placeholder="Where do you want to go ?" /></div>
                     </Col>
                     <Col span={6}>
-                        <div className="gutter-box"> <Input placeholder="Basic usage" /></div>
+                        <div className="gutter-box"> <input type="date"  /></div>
                     </Col>
                     <Col span={6}>
-                        <div className="gutter-box"> <Input placeholder="Basic usage" /></div>
+                        <div className="gutter-box"><button>Search</button></div>
                     </Col>
                     </Row>
                 </div>
