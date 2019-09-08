9a370b2c96456431e199836148d1cba578be0671
laridev@gmail.com <laridev@gmail.com>
2019-09-08 15:35:37 +0100

    adding section of footer

--- src/footer/index.js
+++ src/footer/index.js
@@ -1,13 +1,35 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import { Row, Col } from "antd";
+import './footer.scss';
 class index extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
-    }
+  render() {
+    return (
+      <footer className="footer">
+        <Row gutter={24}>
+          <Col span={6}>
+              <div className="footer-elements">
+
+              </div>
+          </Col>
+          <Col span={6}>
+              <div className="footer-elements">
+
+              </div>
+          </Col>
+          <Col span={6}>
+              <div className="footer-elements">
+
+              </div>
+          </Col>
+          <Col span={6}>
+              <div className="footer-elements">
+
+              </div>
+          </Col>
+        </Row>
+      </footer>
+    );
+  }
 }
 
 export default index;
